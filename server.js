65f9085464af89f01002c450c61b403ff04d86c6
Reetesh Kumar <rajreetesh7@gmail.com>
2022-12-01 01:56:31 +0530

    fixed tofixed error

--- server.js
+++ server.js
@@ -7,14 +7,6 @@ const stripe = require("stripe")(process.env.REACT_APP_STRIPE_PRIVATE_KEY);
 const app = express();
 app.use(cors());
 app.use(express.json());
-const path = require("path");
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "./public/index.html"));
-  });
-}
 
 app.get("/", (req, res) => {
   res.send("welcome to graze");
