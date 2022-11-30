5ac5a426ef3af6d1b4616e7e1a0dd06bf8bfaea8
Reetesh Kumar <rajreetesh7@gmail.com>
2022-11-30 08:50:17 +0530

    fixed starting

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ app.use(cors());
 app.use(express.json());
 const path = require("path");
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  });
+}
+
 app.get("/", (req, res) => {
   res.send("welcome to graze");
 });
